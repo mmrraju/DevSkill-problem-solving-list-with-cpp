4439d31aaee7dc6e183c623d8b93e40a640d6d99
mmrraju <43112579+mmrraju@users.noreply.github.com>
2020-12-25 18:37:49 +0600

    printf() to cout function

--- DCP-184.cpp
+++ DCP-184.cpp
@@ -3,14 +3,13 @@ using namespace std;
 void fib(int x)
 {
     int a=1, b=3, sum=0;
-    //printf("%d %d ", a, b);
     for(int i=3 ; i<=44; i++)
     {
         sum=a+b;
         a=b;
         b=sum;
         if(i==x)
-            printf("%d\n", sum);
+            cout<<sum<<endl;
     }
 
 }
